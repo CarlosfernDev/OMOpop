d1d79d3920156fb980983b9c6aa5761a3dcd4f5c
monikkxd <monicagarciaask@gmail.com>
2023-12-05 03:28:35 +0100

    Script BlockLogic

---
+++ UnityProject/Assets/_Scripts/BlockLogic.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockLogic : MonoBehaviour
+{
+    public int vidaMaxima = 1; // Puedes ajustar la vida inicial del bloque
+    private int vidaActual;
+
+
+    //Funcion publica de restar vida y que compruebe la vida del bloque y otra donde lo haga desaparecer.
+
+    private void Start()
+    {
+        vidaActual = vidaMaxima;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Pelota"))
+        {
+            RestarVida();
+            comprobarVida();
+        }
+    }
+
+    public void RomperBloque()
+    {
+        Debug.Log("Bloque roto");
+        Destroy(gameObject);
+    }
+
+    public void RestarVida()
+    {
+        // Restar vida al bloque
+        vidaActual--;
+    }
+
+    public void comprobarVida()
+    {
+        // Verificar si la vida llegó a cero para romper el bloque
+        if (vidaActual <= 0)
+        {
+            RomperBloque();
+        }
+    }
+}
