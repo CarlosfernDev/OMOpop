402a98c0732f1c67a8dc061b6808b9b681b88b99
stamp <carlosfern010@gmail.com>
2023-12-05 22:31:28 +0100

    Add WebSocket

--- UnityProject/Assets/_Scripts/Blocks/BlockLogic.cs
+++ UnityProject/Assets/_Scripts/Blocks/BlockLogic.cs
@@ -7,6 +7,8 @@ public class BlockLogic : MonoBehaviour
     public int vidaMaxima = 1; // Puedes ajustar la vida inicial del bloque
     private int vidaActual;
 
+    public string ID;
+
 
     //Funcion publica de restar vida y que compruebe la vida del bloque y otra donde lo haga desaparecer.
 
